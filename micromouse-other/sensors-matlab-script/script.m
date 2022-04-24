b4a2135181d4e2f55616bb5f69e1750526a8aff1
lime7git <e.bugajski7@gmail.com>
2022-04-24 14:51:53 +0200

    matlab script for sensors curve fit

--- micromouse-other/sensors-matlab-script/script.m
+++ micromouse-other/sensors-matlab-script/script.m
@@ -1,9 +1,9 @@
-data = importdata('sensors-data.txt', '\t');
+data = importdata('sensors-data-left-side.txt', '\t');
 l = data(:,1);
 v = data(:,2);
 
 F = @(a, data) a(3) + exp(a(1)./(data + a(2)));
-x0 = [2350 205 125];
+x0 = [2585 270 135];
 
 fit = lsqcurvefit(F, x0, l, v)
 
